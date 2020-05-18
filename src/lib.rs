8af56002739d3bbe0e3889aa78cf0073af1c2aa8
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2020-05-17 20:19:04 -0400

    handle padding in b64, fixed xor (chall 2)

--- src/lib.rs
+++ src/lib.rs
@@ -58,18 +58,94 @@ fn test_bits2u8() {
     assert_eq!(bits2u8(&vec!['1', '0', '0', '1', '0', '1']), 37);
 }
 
+fn rpad<T: Clone>(mut values: Vec<T>, desired_length: usize, pad: T) -> Vec<T> {
+    values.extend(std::iter::repeat(pad).take(0.max(desired_length - values.len())));
+    values
+}
+
+#[test]
+fn test_rpad() {}
+
 fn hex2b64(hex: &str) -> String {
-    hex2bits(hex)
+    let mut without_padding = hex2bits(hex)
         .chars()
         .collect::<Vec<char>>()
         .chunks(6)
-        .map(|chunk| base64_table.get(bits2u8(chunk) as usize).expect("wtf"))
+        .into_iter()
+        .map(|chunk| {
+            base64_table
+                .get(bits2u8(&rpad(chunk.to_vec(), 6, '0')) as usize)
+                .unwrap()
+        })
+        .collect::<String>();
+    without_padding.extend(std::iter::repeat(padding).take(without_padding.len() % 4));
+    let with_padding = without_padding;
+    with_padding
+}
+
+#[cfg(test)]
+mod hex2b64_tests {
+    use super::*;
+
+    #[test]
+    fn test_no_padding() {
+        let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+        assert_eq!(hex2b64(hex), b64);
+    }
+
+    #[test]
+    fn test_padding() {
+        let hex = "42";
+        let b64 = "Qg==";
+        assert_eq!(hex2b64(hex), b64);
+    }
+}
+
+fn bits2hex(bits: &str) -> String {
+    assert!(bits.len() % 4 == 0);
+    bits.chars()
+        .collect::<Vec<char>>()
+        .chunks(4)
+        .map(|chunk| match bits2u8(chunk) {
+            0 => '0',
+            1 => '1',
+            2 => '2',
+            3 => '3',
+            4 => '4',
+            5 => '5',
+            6 => '6',
+            7 => '7',
+            8 => '8',
+            9 => '9',
+            10 => 'a',
+            11 => 'b',
+            12 => 'c',
+            13 => 'd',
+            14 => 'e',
+            15 => 'f',
+            _ => unreachable!(),
+        })
         .collect()
 }
 
 #[test]
-fn test_hex2b64() {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    assert_eq!(hex2b64(hex), b64);
+fn test_bits2hex() {}
+
+fn xor(b1: &str, b2: &str) -> String {
+    bits2hex(
+        &hex2bits(b1)
+            .chars()
+            .zip(hex2bits(b2).chars())
+            .map(|(c1, c2)| if c1 == c2 { '0' } else { '1' })
+            .collect::<String>(),
+    )
+}
+
+#[test]
+fn test_xor() {
+    let b1 = "1c0111001f010100061a024b53535009181c";
+    let b2 = "686974207468652062756c6c277320657965";
+    let expected = "746865206b696420646f6e277420706c6179";
+    assert_eq!(xor(b1, b2), expected);
 }
